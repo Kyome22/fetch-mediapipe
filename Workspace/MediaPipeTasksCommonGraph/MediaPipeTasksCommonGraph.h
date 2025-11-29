9c01513b6b6e531d67d5a02b1c1733c2221fad2e
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-11-30 00:27:28 +0900

    Prepared MediaPipeTasksCommonGraph Project

---
+++ Workspace/MediaPipeTasksCommonGraph/MediaPipeTasksCommonGraph.h
@@ -0,0 +1,16 @@
+//
+//  MediaPipeTasksCommonGraph.h
+//  MediaPipeTasksCommonGraph
+//
+//  Created by Takuto Nakamura on 2025/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediaPipeTasksCommonGraph.
+FOUNDATION_EXPORT double MediaPipeTasksCommonGraphVersionNumber;
+
+//! Project version string for MediaPipeTasksCommonGraph.
+FOUNDATION_EXPORT const unsigned char MediaPipeTasksCommonGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaPipeTasksCommonGraph/PublicHeader.h>
